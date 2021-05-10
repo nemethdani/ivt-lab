f860bcc2ac5a6580df3bccb113676f17eebc1783
Daniel Nemeth <nemethdanka@gmail.com>
2021-05-10 15:18:32 +0200

    fireTorpedo_Single_Two_Alternating()

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -50,6 +50,21 @@ public class GT4500Test {
   }
 
   @Test
+  public void fireTorpedo_Single_Two_Alternating(){
+    when(mockPrimaryTorpedoStore.isEmpty()).thenReturn(false);
+    when(mockPrimaryTorpedoStore.fire(1)).thenReturn(true);
+    when(mockSecondaryTorpedoStore.isEmpty()).thenReturn(false);
+    when(mockSecondaryTorpedoStore.fire(1)).thenReturn(true);
+
+    ship.fireTorpedo(FiringMode.SINGLE);
+    ship.fireTorpedo(FiringMode.SINGLE);
+    
+    verify(mockPrimaryTorpedoStore, times(1)).fire(1);
+    verify(mockSecondaryTorpedoStore, times(1)).fire(1);
+  }
+
+
+  @Test
   public void fireTorpedo_All_Success() {
     // Arrange
 
